06892aef77a5317cd2cab5d0273ec4e2f77ae0a5
abdGafar <abdulateef.gafar@gmail.com>
2026-09-08 19:52:05 -0700

    setup: files and working scripts

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # onsset-bio-aff
+Official Open Source Spatial Electrification Tool (OnSSET) analysis on competitive deployment of biomass gasification systems developed using the ``` Global Electrification Platform (GEP) version 3 ```.
+
+
+For more details on our methodology and analysis: -> [Bio-Electrification Potential: A Comparative Approach to Assessing Bioenergy Potential in SSA](http://dx.doi.org/10.2139/ssrn.7031778)
+
+### Setup
+To reproduce OnSSET Bio output
+- Clone git repository main branch and pip install the required dependencies.
+- Create a `/results` folder for GEP Bio output in your root repository.
+- Download required country gep scenario results and place in root repository. This can be obtained from [GEP data repository](https://energydata.info/organization/world-bank-grou?q=GEP&sort=score+desc%2C+metadata_modified+desc).
+- Specify path to the downloaded gep scenario folder in `GEP_Bio.ipynb` notebook.
+- Specify path to crop data file. In our case `/Nigeria_crop.xlsx` 
+- Run all cells in the `GEP_Bio.ipynb` notebook to generate GEP Bio output. Each scenario output file would be generated in `/results/scenarios` folder.
+
+
+### Data
+GEP scenario runs and data for different countries can be obtained from [GEP data repository](https://energydata.info/organization/world-bank-grou?q=GEP&sort=score+desc%2C+metadata_modified+desc) or [Official GEP website](https://electrifynow.energydata.info/).
+
+For our case study: [GEP Nigeria](https://energydata.info/dataset/nigeria-global-electrification-platform-gep).
+
+
+
+## Structure
+### Files
+- `GEP_Bio.ipynb`: main notebook file to run GEP scenarios.
+- `GEP_bio_analysis.ipynb`: notebook file for plotting and analyzing outputs from GEP Bio model.
+- `Nigeria_crop.xlsx`: Contains data on the top energy crop across different states in Nigeria.   
+
+### Folders
+- `/scripts`: contains the `gep_script.py` which has all functions used for GEP Bio analysis.
+- `/results`: contains all output from running GEP Bio. (NB: this folder needs to be created in the root folder after cloning the repo)
+
+### Variables
+
+Electrification Variables
+
+| Variable | Description | Unit / Notes | Input/Output |
+|----------|-------------|--------------|--------------|
+| `PerCapitaDemand` | Per capita energy demand | kWh/capita/yr | Input | 
+| `Pop2030` | Projected population for the year 2030 | person | Input |
+| `HealthDemand2030` | Health sector energy demand in 2030 | kWh/yr | Input |
+| `EducationDemand2030` | Education sector energy demand in 2030 | kWh/yr | Input |
+| `CommercialDemand2030` | Commercial sector energy demand in 2030 | kWh/yr | Input |
+| `AgriDemand2030` | Agricultural energy demand in 2030 | kWh/yr | Input |
+| `TotalEnergyPerCell` | Total energy demand in the population cluster | kWh/yr; calculated as: `(Pop2030 * PerCapitaDemand) ± HealthDemand2030 ± EducationDemand2030 ± CommercialDemand2030 ± AgriDemand2030` | Input |
+| `MinimumOverall2030` | Least cost electrification technology selected | e.g., mini-grid system | Input |
+| `MinimumOverallLCOE2030` | Levelized cost of electrification of the least cost technology | $/kWh | Input |
+| `Generation_CAPEX2030` | Estimated generation capital expenditure (investment cost) of least cost technology in 2030 | $ | Input |
+| `Generation_CAPEX2025` | Estimated generation capital expenditure (investment cost) of least cost technology in 2025 | $ | Input |
+| `X_deg` | Longitude x-coordinates | degrees | Input |
+| `Y_deg` | Latitude y-coordinates | degrees | Input |
+| `id` | Unique identifier per cluster | — | Input |
+| `Tier` | Electricity access tier | — | Input |
+| `Admin1` | Location (State/Province) | — | Input |
+| `Ft_PerTonRes` | Estimated Feedstock price per ton of residues | $/ton | Output: ` main output from GEP Bio model` |
+
+
+
+
+### Reference 
+
+Gafar, A., Zerriffi, H., Ackom, E., Gergel, S. E., & Mentis, D. (2026). Bio-Electrification Potential: A Comparative Approach to Assessing Bioenergy Potential in Sub-Saharan Africa (SSA).
---
+++ scripts/gep_script.py
@@ -0,0 +1,336 @@
+import os
+import logging
+import pandas as pd
+import numpy as np
+import numpy_financial as npf
+import matplotlib.pyplot as plt
+pd.options.mode.chained_assignment = None
+
+#CONSTANTS
+RESULTS_FOLDER = 'results/scenarios'
+
+# MODELLING PARAMETERS
+SYSTEM_EFFICIENCY = 0.3 #System efficiency 30%
+RESIDUE_PRODUCT_RATIO = 1.48 #RPR
+RESIDUE_AVAILABILITY_FACTOR = 0.4 #40% availability
+CROP_YIELD = 2.23 #crop yield in tons/ha
+ANNUAL_HOURS = 8760
+INVESTMENT_COST = 2247 # $/kW
+VARIABLE_OM = 0.002 # Variable operations and maintenance  $/kWh
+DISCOUNT_RATE = 0.07 #Discount rate 7%
+DISCOUNT_FACTOR = 11.65358318 
+
+
+# Function assigns Minimum GEN LCOE
+def get_gen_lcoe(i, gen_sys, sub_data):
+    
+    if gen_sys == 'MG_Wind_Hybrid2030':
+        return sub_data.WindHybridGenLCOE2030.loc[i]
+    
+    elif gen_sys == 'MG_PV_Hybrid2030':
+        return sub_data.PVHybridGenLCOE2030.loc[i]
+    
+    elif gen_sys == 'MG_Hydro2030':
+        return sub_data.MG_Hydro2030.loc[i]
+
+
+# Function takes in scenario files and filter out required variables
+def data_setup(scenario_file):
+    fields = ["GridCellArea","Admin1","Tier","X_deg","Y_deg","id","Pop2030","TotalEnergyPerCell","MinimumOverall2030","MinimumOverallLCOE2030",
+              "WindHybridGenLCOE2030", "PVHybridGenLCOE2030", "MG_Hydro2030", "Generation_Capex2030", "Generation_Capex2025"]
+    
+    data_types_conversion_numeric = {
+                                    "GridCellArea": np.float16,
+                                    "X_deg": np.float16,
+                                    "Y_deg": np.float16,
+                                    "Pop2030": np.float16,
+                                    "MinimumOverallLCOE2030": np.float16,
+                                    "Tier": np.int16,
+                                    "Admin1": "category",
+                                    "MinimumOverall2030": "category"
+                                    }
+    
+    sub_data = pd.read_csv(scenario_file, dtype=data_types_conversion_numeric, skipinitialspace=True, usecols=fields)
+    
+    # Drop non mini-grids
+    sub_data = sub_data.drop(sub_data[(sub_data.MinimumOverall2030 == 'Grid2030') | (sub_data.MinimumOverall2030 == 'SA_PV2030')].index)
+    
+    sub_data = sub_data.drop(sub_data[(sub_data.MinimumOverall2030 == 'MG_Hydro2030')].index)
+
+    sub_data = sub_data.reset_index().drop(columns = 'index')
+    
+    sub_data["MinimumOverallGenLCOE2030"] = [ get_gen_lcoe(i, gen_sys, sub_data) for i, gen_sys in enumerate(sub_data["MinimumOverall2030"])]
+    
+    sub_data['MinimumOverallGenLCOE2030_VOM'] = sub_data['MinimumOverallGenLCOE2030'] - VARIABLE_OM
+    
+    return sub_data
+
+
+# Sets the capacity factor based on the specified tier level
+def get_capacity_factor(tier):
+    
+    if tier == 1 or tier == 2:
+        capacity_factor = (4/24) #Capacity factor (CF)% 4hrs electricity supply daily -> Tier 1,2
+    
+    elif tier == 3:
+        capacity_factor = (8/24) #Capacity factor (CF)% 8hrs electricity supply daily -> Tier 3
+    
+    elif tier == 4:
+        capacity_factor = (16/24) #Capacity factor (CF)% 8hrs electricity supply daily -> Tier 4
+    
+    elif tier == 5:
+        capacity_factor = (23/24) #Capacity factor (CF)% 8hrs electricity supply daily -> Tier 5
+        
+    return capacity_factor
+
+# Get top crop  
+def get_crop(state, crop):
+ 
+    return crop.loc[crop['States'] == state, 'Crop'].iloc[0]
+
+# Get Lower Heating Value (MJ/kg)
+def get_LHV_crop(crop, crop_data):
+    
+    return crop_data.loc[crop_data['Crop'] == crop, 'LHV'].iloc[0]
+
+# Get RPR 
+def get_RPR_crop(crop, crop_data):
+    return crop_data.loc[crop_data['Crop'] == crop, 'RPR'].iloc[0]
+
+# Get Crop Yield value
+def get_yield_crop(crop, crop_data):
+    
+    return crop_data.loc[crop_data['Crop'] == crop, 'CropYield'].iloc[0]
+
+# Get Percentage of Cropping Area value
+def get_percentage_area(state, crop):
+    
+    return crop.loc[crop['States'] == state, 'PercentageArea'].iloc[0]
+
+# Gasifier Capacity 
+def gasifier_parameters(sub_data, scenario):
+    sub_data['CapFactor'] = [get_capacity_factor(c) for c in sub_data['Tier']]
+    
+    sub_data['GasifierCap']=sub_data['TotalEnergyPerCell']/(sub_data['CapFactor']*ANNUAL_HOURS*SYSTEM_EFFICIENCY)
+
+    # Determine Gasifier investment cost in $ (CAPEX), using Investment per kW and GasifierCap
+    sub_data['investment']=sub_data['GasifierCap'] * INVESTMENT_COST
+    
+    return sub_data
+
+
+
+
+'''
+Analyzing crop and residue demand using agricultural production data and Lower heating value (LHV):
+    # Determine the weight of residues in tons
+    # LHV in MJ/kg
+'''
+def crop_res_calc(sub_data, crop, crop_data):
+    
+    # Add crop_data to sub_data
+    sub_data['Crop'] = [ get_crop(state, crop) for state in sub_data['Admin1']]
+    sub_data['PercentageArea'] = [ get_percentage_area(state, crop) for state in sub_data['Admin1']]
+    
+    sub_data['LHV'] = [ get_LHV_crop(crop, crop_data) for crop in sub_data['Crop']]
+    sub_data['RPR'] = [ get_RPR_crop(crop, crop_data) for crop in sub_data['Crop']]
+    sub_data['CropYield'] = [ get_yield_crop(crop, crop_data) for crop in sub_data['Crop']]
+    
+    # Calculate residue weight based on top energy crop LHV
+    sub_data['WeightOfRes']= (sub_data['TotalEnergyPerCell']* 3.6)/(0.3 * sub_data['LHV'] * 1000) #Ton 
+    
+    return sub_data
+
+
+'''
+1.6. Determine the competitive cost for residues
+        
+        NPV(Ft)=  (LCOE* NPV(Et))-NPV(It)-NPV(O&Mt)
+'''
+# Determine NPV of Investment cost (It)
+def npv_investment(sub_data):
+    investArr = sub_data['investment'].to_numpy()
+    npvInvestmentArr = []
+
+    # Working out NPV(It)
+    for i in range(len(investArr)):
+        npvArr = []
+        for j in range(26):
+            if j == 1:
+                npvArr.append(investArr[i])
+            else:
+                npvArr.append(0)
+        npvInvestmentArr.append(npf.npv(DISCOUNT_RATE, npvArr))
+        
+    sub_data['NpvIt']= npvInvestmentArr
+    return sub_data
+
+# Determine NPV Generation (Et)
+def npv_generation(sub_data):
+    genArr = sub_data['TotalEnergyPerCell'].to_numpy()
+    npvGenArr = []
+    
+    # Working out the NPV Gen
+    for i in range(len(genArr)):
+        npvArr = []
+        for j in range(26):
+            if j == 0:
+                npvArr.append(0)
+            else:
+                npvArr.append(genArr[i])
+        npvGenArr.append(npf.npv(DISCOUNT_RATE, npvArr))
+
+    sub_data['NpvGen']= npvGenArr  
+    return sub_data
+
+# Determine NPV Operations and Maintenance (O&Mt) at 5% (0.05) of investment cost
+def npv_operations(sub_data):
+    omArr = (sub_data['investment']* 0.05).to_numpy()
+    npvOmArr = []
+    
+    # Working out NPV Operations
+    for i in range(len(omArr)):
+        npvArr = []
+        for j in range(26):
+            if j == 0:
+                npvArr.append(0)
+            else:
+                npvArr.append(omArr[i])
+        npvOmArr.append(npf.npv(DISCOUNT_RATE, npvArr))
+
+    sub_data['NpvOM']= npvOmArr
+    return sub_data
+
+# Determine NPV of Residue weight (Rt)
+def npv_residue(sub_data):
+    resArr = sub_data['WeightOfRes'].to_numpy()
+    npvResArr = []
+    
+    # Working out the NPV Res
+    for i in range(len(resArr)):
+        npvArr = []
+        for j in range(26):
+            if j == 0:
+                npvArr.append(0)
+            else:
+                npvArr.append(resArr[i])
+        npvResArr.append(npf.npv(DISCOUNT_RATE, npvArr))
+
+    sub_data['NpvRes']= npvResArr  
+    return sub_data
+
+# Determine NPV of Delivered Fuel cost (Ft)
+def npv_fuel_cost(sub_data):
+    sub_data['NpvFt'] = (sub_data['MinimumOverallGenLCOE2030_VOM']*sub_data['NpvGen'])-sub_data['NpvIt']-sub_data['NpvOM']
+
+    # Determining the delivered Fuel cost per Tonne ($/ton)
+    sub_data['Ft_PerTonRes'] = sub_data['NpvFt']/sub_data['NpvRes']
+
+    return sub_data
+
+# Cumulative distribution of delivered fuel costs
+def fuel_CDF(sub_data, scenario, fuelCDF):
+    DeliveredFuelCost = sub_data['Ft_PerTonRes']
+    
+    sorted_df = sub_data.sort_values('Ft_PerTonRes')
+    
+    # Calculate the cumulative proportion of the data that falls below each value
+    if len(DeliveredFuelCost) > 0:
+        cumulative = np.linspace(0,100, len(DeliveredFuelCost))
+        tier = np.array(sorted_df['Tier'])
+        print("Total number of mini-grids", len(DeliveredFuelCost))
+    else:
+        cumulative = []
+        tier = []
+        print("Total number of mini-grids",len(DeliveredFuelCost))
+
+    # Sort the data in ascending order
+    
+    sorted_data = np.sort(DeliveredFuelCost)
+    print(type(sorted_data))
+    
+    # Reassigning index based on length of new dataFrame
+    if len(sorted_data) > len(fuelCDF) and len(fuelCDF) != 0:
+        
+        fuelCDF = fuelCDF.reindex(range(len(sorted_data)))
+    
+    newCDF = pd.DataFrame({f'{scenario}': sorted_data, f'{scenario}-cum': cumulative, f'{scenario}-tier': tier})
+    
+    fuelCDF = pd.concat([newCDF, fuelCDF], axis=1)
+    
+    
+    return fuelCDF
+
+
+
+
+# Post processing
+def data_postprocessing(sub_data, scenario, fuelCDF):
+    outputName = f'{scenario}-results.csv'
+
+    OUTPUT_DIR = os.getcwd() + f"/{RESULTS_FOLDER}"
+    
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+    
+    filename = os.path.join(OUTPUT_DIR, outputName)
+
+    sub_data.to_csv(filename, index=False)
+    
+    fuelCDF.to_csv(os.getcwd() + '/results/CDF.csv', index=False)
+
+    return
+
+# Function assigns Minimum GEN LCOE
+def get_gen_lcoe(i, gen_sys, sub_data):
+    
+    if gen_sys == 'MG_Wind_Hybrid2030':
+        return sub_data.WindHybridGenLCOE2030.loc[i]
+    
+    elif gen_sys == 'MG_PV_Hybrid2030':
+        return sub_data.PVHybridGenLCOE2030.loc[i]
+    
+    elif gen_sys == 'MG_Hydro2030':
+        return sub_data.MG_Hydro2030.loc[i]
+
+# Function assigns Minimum GEN LCOE
+def get_diesel_con(i, gen_sys, sub_data):
+    
+    if gen_sys == 'MG_Wind_Hybrid2030':
+        return sub_data.WindHybridDieselConsumption2030.loc[i]
+    
+    elif gen_sys == 'MG_PV_Hybrid2030':
+        return sub_data.PVHybridDieselConsumption2030.loc[i]
+    
+
+def check_setup(scenario_file):
+    fields =["GridCellArea","Admin1","Tier","X_deg","Y_deg","id","Pop2030","TotalEnergyPerCell","MinimumOverall2030","MinimumOverallLCOE2030",              "WindHybridGenLCOE2030", "PVHybridGenLCOE2030", "MG_Hydro2030", "Generation_Capex2030", "Generation_Capex2025",
+             "MGDieselFuelCost2030","WindHybridDieselConsumption2030", "PVHybridDieselConsumption2030"]
+    
+    data_types_conversion_numeric = {
+                                    "GridCellArea": np.float16,
+                                    "X_deg": np.float16,
+                                    "Y_deg": np.float16,
+                                    "Pop2030": np.float16,
+                                    "MinimumOverallLCOE2030": np.float16,
+                                    "Tier": np.int16,
+                                    "Admin1": "category",
+                                    "MinimumOverall2030": "category"
+                                    }
+    
+    sub_data = pd.read_csv(scenario_file, dtype=data_types_conversion_numeric, skipinitialspace=True, usecols=fields)
+    
+    # Drop non mini-grids
+    sub_data = sub_data.drop(sub_data[(sub_data.MinimumOverall2030 == 'Grid2030') | (sub_data.MinimumOverall2030 == 'SA_PV2030')].index)
+    
+    sub_data = sub_data.drop(sub_data[(sub_data.MinimumOverall2030 == 'MG_Hydro2030')].index)
+
+    sub_data = sub_data.reset_index().drop(columns = 'index')
+    
+    sub_data["MinimumOverallGenLCOE2030"] = [ get_gen_lcoe(i, gen_sys, sub_data) for i, gen_sys in enumerate(sub_data["MinimumOverall2030"])]
+    
+    sub_data["MinOverallDieselCon2030"] = [ get_diesel_con(i, gen_sys, sub_data) for i, gen_sys in enumerate(sub_data["MinimumOverall2030"])]
+    
+    sub_data['MinimumOverallGenLCOE2030_VOM'] = sub_data['MinimumOverallGenLCOE2030'] - VARIABLE_OM
+    
+    return sub_data
